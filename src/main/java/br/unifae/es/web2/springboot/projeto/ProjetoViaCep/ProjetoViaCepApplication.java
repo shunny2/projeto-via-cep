82cc8ef3b8f23baef3613f5667306383bafe828b
Alexander Davis <72872854+shunny2@users.noreply.github.com>
2021-09-10 12:04:09 -0300

    Commit inicial

---
+++ src/main/java/br/unifae/es/web2/springboot/projeto/ProjetoViaCep/ProjetoViaCepApplication.java
@@ -0,0 +1,13 @@
+package br.unifae.es.web2.springboot.projeto.ProjetoViaCep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoViaCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoViaCepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/unifae/es/web2/springboot/projeto/ProjetoViaCep/ProjetoViaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unifae.es.web2.springboot.projeto.ProjetoViaCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoViaCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
